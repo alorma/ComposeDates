9ffbd1feca59db6aaa3d9e63ee06c25d62602827
Bernat Borrás <bernat.borras@adevinta.com>
2020-12-28 16:01:00 +0100

    Cleanup

--- app/src/main/java/com/alorma/dates/domain/TimeDistanceCalculator.kt
+++ app/src/main/java/com/alorma/dates/domain/TimeDistanceCalculator.kt
@@ -8,30 +8,22 @@ class TimeDistanceCalculator(private val clock: Clock) {
     private val clockTime: LocalDateTime = LocalDateTime.now(clock)
 
     fun calculateTimeDistance(localDateTime: LocalDateTime): TimeCalculation {
-        val period = Period.between(localDateTime.toLocalDate(), clockTime.toLocalDate()).let {
-            if (it.isNegative) {
-                it.negated()
-            } else {
-                it
-            }
+        if (localDateTime.isEqual(clockTime)) {
+            return TimeCalculation.Now
         }
+        val period = getPeriod(localDateTime)
 
         val years = period.years
         val months = period.months
         val days = period.days
 
-        val duration = Duration.between(localDateTime.toLocalTime(), clockTime.toLocalTime()).abs()
-
-        val midNightPlus = LocalTime.MIDNIGHT.plus(duration)
+        val midNightPlus = getDuration(localDateTime)
 
         val hours = midNightPlus.get(ChronoField.HOUR_OF_DAY)
         val minutes = midNightPlus.get(ChronoField.MINUTE_OF_HOUR)
         val seconds = midNightPlus.get(ChronoField.SECOND_OF_MINUTE)
 
         return when {
-            localDateTime.isEqual(clockTime) -> {
-                TimeCalculation.Now
-            }
             localDateTime.isAfter(clockTime) -> {
                 TimeCalculation.Elapsed.After(years, months, days, hours, minutes, seconds)
             }
@@ -39,6 +31,21 @@ class TimeDistanceCalculator(private val clock: Clock) {
                 TimeCalculation.Elapsed.Before(years, months, days, hours, minutes, seconds)
             }
             else -> error("Invalid time calculation")
+        }
+    }
+
+    private fun getDuration(localDateTime: LocalDateTime): LocalTime {
+        val duration = Duration.between(localDateTime.toLocalTime(), clockTime.toLocalTime()).abs()
+        return LocalTime.MIDNIGHT.plus(duration)
+    }
+
+    private fun getPeriod(localDateTime: LocalDateTime): Period {
+        return Period.between(localDateTime.toLocalDate(), clockTime.toLocalDate()).let {
+            if (it.isNegative) {
+                it.negated()
+            } else {
+                it
+            }
         }
     }
 
